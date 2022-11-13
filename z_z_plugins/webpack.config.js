01f6dec7f7797090320e4d4293a9c1c343dad918
Cyril- <1978919203@qq.com>
2022-11-13 20:14:18 +0800

    feat:plugin-init

---
+++ z_z_plugins/webpack.config.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: Cyril- 1978919203@qq.com
+ * @Date: 2022-11-13 20:05:49
+ * @LastEditors: Cyril- 1978919203@qq.com
+ * @LastEditTime: 2022-11-13 20:13:47
+ * @FilePath: /webpack_study/z_z_plugins/webpack.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/[name].js',
+    clean: true,
+  },
+  module: {
+
+  },
+  plugins: [
+
+  ],
+  mode: "development"
+}
